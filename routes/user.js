5c5fa7d9d1097d6882d2f7f2ec3913ea86104733
beardgod7 <nwaokefrancis@gmail.com>
2023-11-17 23:22:14 +0100

    first commit

--- routes/user.js
+++ routes/user.js
@@ -128,5 +128,24 @@ router.post(
     }
   })
 );
+router.get(
+  "/getuser",
+  //sisAuthenticated,
+  catchAsyncErrors(async (req, res, next) => {
+    try {
+      const user = await User.findById(req.user.id);
 
+      if (!user) {
+        return next(new ErrorHandler("User doesn't exists", 400));
+      }
+
+      res.status(200).json({
+        success: true,
+        user,
+      });
+    } catch (error) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  })
+);
 module.exports=router
