d8aba029b84c5a7f6cb3494be29f716723d34479
beardgod7 <nwaokefrancis@gmail.com>
2023-11-18 00:00:14 +0100

    first commit

--- routes/user.js
+++ routes/user.js
@@ -129,19 +129,17 @@ router.post(
   })
 );
 router.get(
-  "/getuser",
-  //sisAuthenticated,
+  "/admin-all-users",
+  //isAuthenticated,
+ // isAdmin("Admin"),
   catchAsyncErrors(async (req, res, next) => {
     try {
-      const user = await User.findById(req.user.id);
-
-      if (!user) {
-        return next(new ErrorHandler("User doesn't exists", 400));
-      }
-
-      res.status(200).json({
+      const users = await User.find().sort({
+        createdAt: -1,
+      });
+      res.status(201).json({
         success: true,
-        user,
+        users,
       });
     } catch (error) {
       return next(new ErrorHandler(error.message, 500));
